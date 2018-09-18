29a376d4ae2d17d47671cc62d4564f22b81b80e9
fleig <lucas.fleig@gmail.com>
2018-09-18 00:33:21 -0300

    MaxHeap and HeapSort

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,6 +8,24 @@ public class Main {
 	// write your code here
     }
 
+    void HeapSort(List<Integer> list){
+        buildMaxHeap(list);
+
+        for(int i = list.size()-1; i>0; i--){
+            int aux = list.get(0);
+            list.set(0, list.get(i));
+            list.set(i, aux);
+        }
+    }
+
+    void buildMaxHeap(List<Integer> list){
+        int size = list.size() - 1;
+
+        for(int i = size/2; i >= 0; i--){
+            maxHeapify(list, i);
+        }
+    }
+
     void maxHeapify(List<Integer> list, int index)
     {
         int left = 2*index;
@@ -30,7 +48,6 @@ public class Main {
             int aux = list.get(index);
             list.set(index, list.get(max));
             list.set(max, aux);
-
 
             maxHeapify(list, max);
         }
