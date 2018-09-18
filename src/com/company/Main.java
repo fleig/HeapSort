cf04586ed97fd7c56e4d93faaed5878b4b40fbe8
fleig <lucas.fleig@gmail.com>
2018-09-18 01:12:57 -0300

    fixing MaxHeapify and HeapSort

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,24 +1,48 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Stack;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(57,205,342,200,197,946,631,92,66,581,345,220,398,249,329,87,186,144,462,431));
+        heapSort(list);
     }
 
-    void HeapSort(List<Integer> list){
+    static void heapSort(List<Integer> list){
+        Stack<Integer> pilha = new Stack<>();
+
         buildMaxHeap(list);
 
-        for(int i = list.size()-1; i>0; i--){
+        int size = list.size() - 1;
+
+        for(int i = size; i>=0; i--){
+            //swap
             int aux = list.get(0);
             list.set(0, list.get(i));
             list.set(i, aux);
+
+            pilha.push(list.get(i));
+            list.remove(i);
+
+            maxHeapify(list, 0);
         }
+
+        print(pilha);
     }
 
-    void buildMaxHeap(List<Integer> list){
+    static void print(Stack<Integer> pilha){
+        System.out.print("List: ");
+        while(pilha.size() > 0) {
+            System.out.print(pilha.pop() + " ");
+        }
+        System.out.println();
+    }
+
+    static void buildMaxHeap(List<Integer> list){
         int size = list.size() - 1;
 
         for(int i = size/2; i >= 0; i--){
@@ -26,19 +50,18 @@ public class Main {
         }
     }
 
-    void maxHeapify(List<Integer> list, int index)
-    {
+    static void maxHeapify(List<Integer> list, int index){
         int left = 2*index;
         int right = 2*index + 1;
 
         int max = index;
 
         // Se filho da esquerda maior que max
-        if (left <= list.size() && list.get(left) > list.get(max))
+        if (left < list.size() && list.get(left) > list.get(max))
             max = left;
 
         // Se filho da direita maior que max
-        if (right <= list.size() && list.get(right) > list.get(max))
+        if (right < list.size() && list.get(right) > list.get(max))
             max = right;
 
         // se max nao for raiz
